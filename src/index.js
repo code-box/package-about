ed80d824d333ea50fc5de5a42a47eaa120c82d84
Rob Moran <rob.moran@arm.com>
2015-07-02 15:40:34 +0100

    Added Help menubar registration to about package

--- src/index.js
+++ src/index.js
@@ -97,3 +97,30 @@ codebox.app.once("ready", function() {
     about();
 });
 
+codebox.menubar.createMenu({
+    id: "help",
+    caption: "Help",
+    position: 100,
+    items: [
+        {
+            caption: "About",
+            command: "application.about"
+        },
+        {
+            caption: "Welcome",
+            command: "application.welcome"
+        },
+        {
+            caption: "Releases Notes",
+            command: "application.changes"
+        },
+        {
+            caption: "Documentation",
+            command: "application.help"
+        },
+        {
+            caption: "Send Feedback",
+            command: "application.feedback"
+        }
+    ]
+});
